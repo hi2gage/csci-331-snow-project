fc5121a1aca9f113a1015c450921060a4727f5f3
Gage Halverson <gagehalverson@iMac.lan>
2021-10-25 14:05:33 -0600

    added react without Docker

--- server/app.js
+++ server/app.js
@@ -10,7 +10,13 @@ app.get('/', function (req, res) {
   res.send('Now isnt this great. This is a real test')
 })
 
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 //Launch listening server on port 8080
 app.listen(process.env.PORT || 5000, function () {
   console.log('app listening on port 5000 or whatever you like!')
 })
+
