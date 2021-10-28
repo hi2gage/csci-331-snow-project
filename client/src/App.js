4e478f7d54c1a45dc2537b91f03c4d0ca2d4e5df
Gage Halverson <gagehalverson@iMac.lan>
2021-10-28 13:46:21 -0600

    fixed front end errors and got cors to work properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Timepicker from './timepicker/Time-picker';
 import React from "react";
-import { useState } from 'react';
-import TimePicker from 'react-time-picker/dist/entry.nostyle'
+
+
 
 
 function App() {
   const [data, setData] = React.useState(null);
-  const [value, onChange] = useState('10:00');
+
 
   React.useEffect(() => {
     fetch("/api")
@@ -21,7 +21,6 @@ function App() {
       <div class="flex h-screen ">
         <div class="m-auto text-grey-1000 font-">
           <h3>{data}</h3>
-          {/* <button>button</button> */}
           <div class="border-solid border-red-500 x-10 p-6"><Timepicker /></div>
           <div class="border-solid border-red-500 x-10 p-6"><Timepicker /></div>
           <div class="border-solid border-red-500 x-10 p-6"><Timepicker /></div>
