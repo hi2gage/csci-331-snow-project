180d13907b6a0de0c588673a2b84c2b9a82536c4
Gage Halverson <gagehalverson@iMac.lan>
2021-11-01 22:00:20 -0600

    fixed the local selection problem

--- server/app.js
+++ server/app.js
@@ -1,94 +1,63 @@
 //Load express module with `require` directive
-var express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+require("dotenv").config();
 
-var dbAcess = require('./database/dbAccess')
+var dbAcess = require("./database/dbAccess");
 
 const { Pool, Client } = require("pg");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-if(!process.env.LOCAL_OR_HEROKU === 'local') {
-  const credentials = {
-    user: "postgres",
-    host: "db",
-    database: "mainDB",
-    password: "pass",
-    port: 5432,
-  };
-  
-  const pool = new Pool(credentials);
-  console.log(process.env)
-}
+var app = express();
 
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
-
-
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-
-
-var app = express()
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
-app.disable('etag');
+app.disable("etag");
 
 // Setting up app to use JSON
 app.use(express.json());
 
-
-
-
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send(process.env.LOCAL_OR_HEROKU)
-})
-
-
+app.get("/", function (req, res) {
+  res.send(process.env.LOCAL_OR_HEROKU);
+});
 
 app.get("/api", (req, res) => {
-
-  res.json({hour: '00', minute: '00', snow: 0});
+  res.json({ hour: "00", minute: "00", snow: 0 });
 });
 
 app.get("/apidb", (req, res) => {
-
   // dbAcess.getAll().then(data => {
   //   console.log(data);
   //   res.json(data);
   // });
-  
-  dbAcess.getAll().then(data => {
+
+  dbAcess.getAll().then((data) => {
+    // res.send(process.env.LOCAL_OR_HEROKU);
     res.send(JSON.stringify(data.rows, null, "   "));
   });
-
-
-  
 });
 
-
-
-
 app.put("/api", (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
+  res.setHeader("Content-Type", "text/plain");
+  res.write("you posted:\n");
+  res.end(JSON.stringify(req.body, null, 2));
 
   var data = req.body;
 
-  console.log("\n------JSON-------")
-  console.log(JSON.stringify(data, null, 2))
-  
+  console.log("\n------JSON-------");
+  console.log(JSON.stringify(data, null, 2));
+
   console.log(data);
 });
 
 //Launch listening server on port 8080
 app.listen(process.env.PORT || 5000, function () {
-  console.log('app listening on port 5000 or whatever you like!')
-})
-
+  console.log("app listening on port 5000 or whatever you like!");
+});
--- server/database/dbAccess.js
+++ server/database/dbAccess.js
@@ -2,50 +2,41 @@ const e = require("express");
 const { Pool, Client } = require("pg");
 
 const credentials = {
-    user: "postgres",
-    host: "db",
-    database: "mainDB",
-    password: "pass",
-    port: 5432,
-    };
+  user: "postgres",
+  host: "db",
+  database: "mainDB",
+  password: "pass",
+  port: 5432,
+};
 
 async function getAll(personId) {
-    if(!process.env.LOCAL_OR_HEROKU === 'local') {
-        const pool = new Pool(credentials);
-        const text = `SELECT * FROM times ORDER BY id ASC`;
-        return pool.query(text);
-    }
-    else {
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-              rejectUnauthorized: false
-            }
-          });
-          
-          client.connect();
-          
-          client.query('SELECT * FROM times ORDER BY id ASC;', (err, res) => {
-            if (err) throw err;
-            for (let row of res.rows) {
-              console.log(JSON.stringify(row));
-            }
-            client.end();
-            return res;
-          });
-    }
+  console.log(process.env.LOCAL_OR_HEROKU);
+  if (process.env.LOCAL_OR_HEROKU == "local") {
+    console.log("We are on local");
+    const pool = new Pool(credentials);
+    const text = `SELECT * FROM times ORDER BY id ASC`;
+    return pool.query(text);
+  
+} 
+  else {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
 
-    
+    client.connect();
 
-    
-
-    
-
-
-
-    
+    client.query("SELECT * FROM times ORDER BY id ASC;", (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+      return res;
+    });
+  }
 }
-    
 
-
-module.exports = {getAll };
+module.exports = { getAll };
