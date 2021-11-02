9c4f2a4ef7cfc1c4f8cc3c81c0759544daf38042
Gage Halverson <gagehalverson@iMac.lan>
2021-11-01 21:42:51 -0600

    this will actually work

--- server/database/dbAccess.js
+++ server/database/dbAccess.js
@@ -1,8 +1,7 @@
+const e = require("express");
 const { Pool, Client } = require("pg");
 
-if(!process.env.LOCAL_OR_HEROKU === 'local') {
-
-    const credentials = {
+const credentials = {
     user: "postgres",
     host: "db",
     database: "mainDB",
@@ -10,40 +9,43 @@ if(!process.env.LOCAL_OR_HEROKU === 'local') {
     port: 5432,
     };
 
-    async function poolDemo() {
-        const pool = new Pool(credentials);
-        const now = await pool.query("SELECT NOW()");
-        await pool.end();
-        
-        return now;
-    }
-
-    async function getAll(personId) {
+async function getAll(personId) {
+    if(!process.env.LOCAL_OR_HEROKU === 'local') {
         const pool = new Pool(credentials);
         const text = `SELECT * FROM times ORDER BY id ASC`;
         return pool.query(text);
     }
+    else {
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false
+            }
+          });
+          
+          client.connect();
+          
+          client.query('SELECT * FROM times ORDER BY id ASC;', (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+              console.log(JSON.stringify(row));
+            }
+            client.end();
+            return res;
+          });
+    }
 
-} 
-else {
-    const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
-      });
-      
-      client.connect();
-      
-      client.query('SELECT * FROM times ORDER BY id ASC;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          console.log(JSON.stringify(row));
-        }
-        client.end();
-        return res;
-      });
+    
+
+    
+
+    
+
+
+
+    
 }
+    
 
 
-module.exports = { poolDemo, getAll };
+module.exports = {getAll };
