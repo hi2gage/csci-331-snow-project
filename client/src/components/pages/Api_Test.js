faf742972057f1c294042da38394ae6beff9dffe
Gage Halverson <gage@Gages-MBP.lan>
2021-11-03 14:44:15 -0600

    added a testing page

---
+++ client/src/components/pages/Api_Test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Timepicker from "./timepicker/Time-picker";
+import React, { useState, useEffect} from 'react'
+
+
+
+function Api_Test() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Api-Test
+
