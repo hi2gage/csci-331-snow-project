cbb16889252f2e3d1d3374afd5b3a5a8e49a09ec
jikerr <kerrjustin15@gmail.com>
2021-11-03 13:05:58 -0600

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import Timepicker from "./timepicker/Time-picker";
-import React from "react";
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -15,22 +20,33 @@ function App() {
     }, [data]);
 
     return (
-        <div className="bg-gray-200 bg-h-min text-center min-h-screen">
-            <div className="flex h-screen ">
-                <div className="m-auto text-grey-1000 font-">
-                    <h3>{data}</h3>
-                    <div className="border-solid border-red-500 x-10 p-6">
-                        <Timepicker />
-                    </div>
-                    <div className="border-solid border-red-500 x-10 p-6">
-                        <Timepicker />
-                    </div>
-                    <div className="border-solid border-red-500 x-10 p-6">
-                        <Timepicker />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/services' component={Services}/>
+                <Route path='/products' component={Products}/>
+                <Route path='/sign-up'  component={SignUp}/>
+                </Switch>
+            </Router>     
+        </>
+        // <div className="bg-gray-200 bg-h-min text-center min-h-screen">
+        //     <div className="flex h-screen ">
+        //         <div className="m-auto text-grey-1000 font-">
+        //             <h3>{data}</h3>
+        //             <div className="border-solid border-red-500 x-10 p-6">
+        //                 <Timepicker />
+        //             </div>
+        //             <div className="border-solid border-red-500 x-10 p-6">
+        //                 <Timepicker />
+        //             </div>
+        //             <div className="border-solid border-red-500 x-10 p-6">
+        //                 <Timepicker />
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
