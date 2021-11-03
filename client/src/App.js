7d0e3499181d7330baca19ee3f160769d16a18e8
Gage Halverson <gagehalverson@iMac.lan>
2021-11-03 12:33:23 -0600

    main

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
-        fetch("/api")
+        fetch("/apidb")
             .then((res) => res.json())
             .then((data) => setData(data.message))
             .then(console.log(data));
