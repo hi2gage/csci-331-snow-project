899a259a2c846c11c09d2aa97427a778de607955
Gage Halverson <gage@Gages-MBP.lan>
2021-11-04 00:15:06 -0600

    figured out the state of updating from either the forum or the prop

--- client/src/components/pages/Api_Test.js
+++ client/src/components/pages/Api_Test.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 // import '../../App.css';
 import '../../index.css';
 import axios from 'axios';
+import Timepicker from '../../timepicker/Time-picker'
 
 
 
@@ -23,42 +24,49 @@ function Api_Test() {
 
     function table() {
         return (
-            <table className="px-2 py-2 border-2 border-yellow-800">
-                <tr>
-                    <th>Snow</th>
-                    <th>Hour</th>
-                    <th>Minute</th>
-                </tr>
-                <tr>
-                    <td>{data[0].snow}</td>
-                    <td>{data[0].hr}</td>
-                    <td>{data[0].min}</td>
-                </tr>
-                <tr>
-                    <td>{data[1].snow}</td>
-                    <td>{data[1].hr}</td>
-                    <td>{data[1].min}</td>
-                </tr>
-                <tr>
-                    <td>{data[2].snow}</td>
-                    <td>{data[2].hr}</td>
-                    <td>{data[2].min}</td>
-                </tr>
-            </table>
+            <div className="px-10 py-2">
+                <table className="px-10 py-2 table-auto text-center">
+                    <tr>
+                        <th className="w-6 px-5 py-2 border-2 border-green-800">Snow</th>
+                        <th className="w-6 px-5 py-2 border-2 border-green-800">Hour</th>
+                        <th className="w-6 px-5 py-2 border-2 border-green-800">Minute</th>
+                    </tr>
+                    <tr>
+                        <td>{data[0].snow}"</td>
+                        <td>{data[0].hr}</td>
+                        <td>{data[0].min}</td>
+                    </tr>
+                    <tr>
+                        <td>{data[1].snow}"</td>
+                        <td>{data[1].hr}</td>
+                        <td>{data[1].min}</td>
+                    </tr>
+                    <tr>
+                        <td>{data[2].snow}"</td>
+                        <td>{data[2].hr}</td>
+                        <td>{data[2].min}</td>
+                    </tr>
+                </table>
+            </div>
         )
     }
 
-
+    {/* <div class="flex h-screen justify-center items-center">
+    <div class="text-center bg-blue-400"> <!-- ⬅️ THIS DIV WILL BE CENTERED -->
+        <h1 class="text-3xl">HEADING</h1>
+        <p class="text-xl">Sub text</p>
+    </div>
+  </div> */}
 
 
 
     return (
-        <div className="flex h-screen">
-            <div>
-                <div className='text-purple-600 border-4 border-red-300'>THIS IS the API Test</div>
-                <div className='text-purple-600 border-4 border-red-900'>THIS IS the API Test</div>
-                <div>{(data == null) ? 'not loaded' : table()}</div>
-
+        <div className="flex h-screen justify-center items-center px-10 py-10">
+            <div className="">
+                <div className='text-3xl border-2 border-yellow-800'>{(data == null) ? 'not loaded' : table()}</div>
+                
+                <div className='text-3xl'>{(data == null) ? 'not loaded' : <Timepicker hour={'0' + data[0].hr.toString()} minute={data[0].min.toString()} />}</div>
+                
             </div>
 
 
