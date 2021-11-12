f5db292268369bc43ceb41b29c93b8336bf5c9a0
jikerr <kerrjustin15@gmail.com>
2021-11-12 16:44:36 -0700

    small css and js changes

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -11,7 +11,7 @@ function HeroSection() {
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch A Dope Vid <i className='far fa-play-circle' /></Button>
+                {/* <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch A Dope Vid <i className='far fa-play-circle' /></Button> */}
             </div>
         </div>
     )
