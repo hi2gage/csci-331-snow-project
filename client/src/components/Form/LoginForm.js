277f51bb9ecfa2bcf6d4eced340e1d86dbad695c
jikerr <kerrjustin15@gmail.com>
2021-11-22 14:43:05 -0700

    Login Form design

--- client/src/components/Form/LoginForm.js
+++ client/src/components/Form/LoginForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './LoginForm.css'
 
 function LoginForm({Login, error}) {
     
@@ -15,16 +16,16 @@ function LoginForm({Login, error}) {
             <div className="form-inner">
                 <h2>Login</h2>
                 {((error != "") ? (<div className="error">{error}</div>) : "" )}
-                <div>
+                <div className="form-group">
                     <label htmlFor="name">Name: </label>
                     <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
                 </div>
-                <div className="for-group">
+                <div className="form-group">
                     <label htmlFor="email">Email: </label>
                     <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
                 </div>
-                <div className="for-group">
-                    <label htmlFor="password">password: </label>
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
                     <input type="text" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
                 </div>
                 <input type="submit" value="LOGIN"/>
